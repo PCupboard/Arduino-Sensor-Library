bd50807f27a9b5bc4fe6af5df3fc849e1f6747ae
PCupboard <PatrickJemieljanczyk@gmail.com>
2025-11-23 16:24:49 +0100

    Added Tadas' Keypad library

---
+++ templates/keypad_template/Keypad.cpp
@@ -0,0 +1,119 @@
+#include "components.h"
+
+struct PinPair {
+    int readPin;
+    int drivePin;
+};
+
+PinPair keypadLayout[12];
+const char keys[] = {'1','2','3','4','5','6','7','8','9','*','0','#'};
+
+
+void Keypad::startUp(int amount, int keyPins[]) {
+    this->amount = amount;
+
+    for (int i = 0; i < amount; i++) {
+        pins[i] = keyPins[i];
+        pinMode(pins[i], INPUT_PULLUP);
+    }
+}
+
+// -- These 2 define the pins, and makes them INPUT_PULLUP using the code above. >:!
+void Keypad::define(int p1,int p2,int p3,int p4,int p5,int p6,int p7) {
+    int temp[] = {p1,p2,p3,p4,p5,p6,p7};
+    startUp(7, temp);
+}
+
+int Keypad::getAmount() { return amount; }
+
+
+int Keypad::getPin(int index) {
+    if (index < 0 || index >= amount) return -1;
+    return pins[index];
+}
+
+void Keypad::keyMaping() {
+    
+    Serial.println("Mapping started, follow the instructions to map keypad correctly:");
+    
+    for (int i = 0; i < 12; i++) {
+        char key = keys[i];
+        Serial.println("Please press: ");
+        Serial.print(key);
+
+        // ---- This bad boy, uhh, he let's the human brain catch up, by waiting for input. ----
+        // --- (thease are indexes not actuall pin nr) ---
+        bool keyScanned = false;
+
+        while (!keyScanned) {           
+            const int count = getAmount();
+          
+            for (int currentPin = 0; currentPin < count; currentPin++) {
+
+                // ---- Changes the pins to the states i want (thease are indexes not actuall pin nr) ----
+                for (int testingPin = 0; testingPin < count; testingPin++) {
+                    int actuallPin = getPin(testingPin);
+                
+                    if (testingPin == currentPin) {
+                        pinMode(actuallPin, OUTPUT);
+                        digitalWrite(actuallPin, LOW);
+                    } else {
+                        pinMode(actuallPin, INPUT_PULLUP);
+                    }
+                }
+
+                // ---- Where the actual testing happens if pins are connected when button pressed in
+                for (int testingPin = 0; testingPin < count; testingPin++) {
+                    if (testingPin == currentPin) continue;
+                    int readPin = getPin(testingPin);
+
+                    if (digitalRead(readPin) == LOW) {
+                        int drivePin = getPin(currentPin);
+
+                        // The actaully mapping
+                        keypadLayout[i].readPin  = readPin;
+                        keypadLayout[i].drivePin = drivePin;
+
+                        keyScanned = true;
+
+                        while (digitalRead(readPin) == LOW) {
+                            delay(5);
+                        }
+                        break;
+                    }
+                }
+
+                if (keyScanned) break;
+            }
+
+            delay(5);
+        }
+    }
+}
+
+char Keypad::getKey(){
+    const int count = getAmount();
+
+    startUp(count, pins);
+
+    for (int i = 0; i < 12; i++) {
+        const int drivePin = keypadLayout[i].drivePin;
+        const int readPin  = keypadLayout[i].readPin;
+
+        pinMode(drivePin, OUTPUT);
+        digitalWrite(drivePin, LOW);
+
+        if (digitalRead(readPin) == LOW) {
+            while (digitalRead(readPin) == LOW) {
+                delay(5);
+            }
+
+            pinMode(drivePin, INPUT_PULLUP);
+            return keys[i];  
+        }
+
+        pinMode(drivePin, INPUT_PULLUP);
+    }
+
+    return 0;  // no key pressed
+}
---
+++ templates/keypad_template/components.h
@@ -0,0 +1,36 @@
+// LIBRARY CREATED BY TADAS
+
+#ifndef COMPONENTS_H
+#define COMPONENTS_H
+
+#include <Arduino.h>
+extern HardwareSerial Serial;
+
+class Keypad {
+    int amount = 0;
+    int pins[7] = {};
+
+    // ---- Code to initialize pins as ---- Used in corelation with define ---- 
+    void startUp(int amount, int keyPins[]);
+public:
+
+    void define(int p1,int p2,int p3,int p4,int p5,int p6,int p7);
+
+    int getAmount();
+    int getPin(int index);
+
+    void keyMaping();
+    char getKey();
+};
+
+// Frontend (empty logic for now)
+class Frontend {
+private:
+    Keypad &kp;
+public:
+    Frontend(Keypad &ref);
+    void init();
+    void update();
+};
+
+#endif
